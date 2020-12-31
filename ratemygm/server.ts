dd67fc7e3efa120f1f1d44c200171617ee46ff1e
Megan Starr <megan_starr92@hotmail.com>
2020-12-31 04:15:49 -0600

    undo broken game master libraries (need to move to separate repo)

--- ratemygm/server.ts
+++ ratemygm/server.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from 'express';
 import session from 'express-session';
 
-import {
+/*import {
   submitAuth,
   viewAuth,
   destroyAuth,
   submitRegister,
   viewRegister,
-} from '@techofmany/user';
+} from '@techofmany/user';*/
 
 const app = express();
 const port = 3002;
@@ -21,11 +21,11 @@ app.use(session({
 app.get('/', (req: Request, res: Response) => {
   res.send('ratemygamemaster.com')
 });
-app.post('/login', submitAuth);
+/*app.post('/login', submitAuth);
 app.use('/login', viewAuth);
 app.post('/register', submitRegister);
 app.use('/register', viewRegister);
-app.get('/logout', destroyAuth);
+app.get('/logout', destroyAuth);*/
 
 app.listen(port, () => {
   console.log(`Example app listening at on port ${port}`)
