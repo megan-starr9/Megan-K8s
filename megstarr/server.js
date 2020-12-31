9519bce97180770afd25ce2abb24b97465a5b8d2
Megan Starr <megan_starr92@hotmail.com>
2020-12-31 02:46:39 -0600

    resume site!!! (and initial user stuff)

--- megstarr/server.js
+++ megstarr/server.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Set render engine
+app.set( 'view engine', 'twig' );
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('megstarr.com')
+  res.render('index.twig');
+})
+
+app.get('/projects', (req, res) => {
+  res.render('projects.twig');
+})
+
+app.get('/resume', (req, res) => {
+  res.render('resume.twig');
 })
 
 app.listen(port, () => {
